865a64075eea77ce6371f2b18f2dc6961c6a7177
oumeima <oumeima.baj2023@gmail.com>
2026-04-10 05:24:15 +0100

    ;

--- back/src/controllers/workflowController.js
+++ back/src/controllers/workflowController.js
@@ -131,7 +131,6 @@ exports.createWorkflow = async (req, res) => {
       });
     }
 
-    const workflowDomain = domain || req.user.domain;
     const currentUserId = req.user.id || req.user.userId || req.user._id;
     const workflowDomain = domain || req.user.domain || "General";
     let workflowNodes = nodes || [];
